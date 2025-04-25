b0ac0f73d1fe89a4fec49675105c9595d23d2be9
eliagarcia <elia.garcia.rovira@gmail.com>
2025-04-25 12:39:01 +0200

    exercicis de laboratori

--- backend/battleship/api/urls.py
+++ backend/battleship/api/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import PlayerViewSet, GameViewSet
 
 # Create a router and register our ViewSets with it.
 router = DefaultRouter()
 
 router.register(r'user', views.UserViewSet, basename='user')
+
+router.register(r'players', PlayerViewSet)
+router.register(r'games', GameViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- backend/battleship/api/views.py
+++ backend/battleship/api/views.py
@@ -1,6 +1,13 @@
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from . import serializers
+from rest_framework import viewsets, filters, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+from .models import Player, Game
+from .serializers import PlayerSerializer, GameSerializer
 
 
 
@@ -10,4 +17,21 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nickname']
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['phase']
+    ordering_fields = ['id']
+
+    def perform_create(self, serializer):
+        player = get_object_or_404(models.Player, user=User.objects.first())
+        serializer.save(owner=player)
 
